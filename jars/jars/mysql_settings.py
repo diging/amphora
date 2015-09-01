16db8ec2f60d54bd60e61b074ff253e2b383b36a
Erick Peirson <erick.peirson@asu.edu>
2015-09-01 07:23:56 -0700

    added static_root

--- jars/jars/mysql_settings.py
+++ jars/jars/mysql_settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/wwwnginx/jars/static/'
 MEDIA_ROOT = './jars/'
 MEDIA_URL = '/'
 
