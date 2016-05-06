eb6be0b397da028ec977833cbba939171fe7ad9b
Erick Peirson <erick.peirson@asu.edu>
2016-05-06 15:56:12 -0400

    config

--- jars/settings.py
+++ jars/settings.py
@@ -133,10 +133,18 @@ WSGI_APPLICATION = 'jars.wsgi.application'
 #     }
 # }
 # else:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/diging/applications/jars/mysql.cnf',
+        },
     }
 }
 
