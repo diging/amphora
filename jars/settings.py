68e9b6a47e61ce3f0a264f8bca9f7e51ead1751c
Erick Peirson <erick.peirson@asu.edu>
2016-05-06 15:43:35 -0400

    minor

--- jars/settings.py
+++ jars/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/amphora/static/'
 STATIC_ROOT = 'static/'
 
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', 'media')
